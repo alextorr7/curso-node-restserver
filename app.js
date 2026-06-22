40a36c01a46c28de5547bc95c32c913e92892812
Alexander <silvino.torres@unmsm.edu.pe>
2026-06-21 22:39:34 -0500

    Primer Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+/*const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto', process.env.PORT);
+});*/
+
+require('dotenv').config();
+
+const Server = require('./models/server');
+
+const server = new Server();
+
+server.listen();
